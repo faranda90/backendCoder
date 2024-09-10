019e9b1aaa5f35783e7389925f3da04355a555cb
Facundo Aranda <facundor.aranda@gmail.com>
2024-09-10 00:35:57 -0300

    Segundo Commit

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -27,5 +27,23 @@ router.get('/:id',async(req,res)=>{
 })
 
 
+router.post('/',async(req,res)=>{
+    
+    const body = req.body
+    const data = await fs.promises.readFile('products.json','utf-8')
+    const products = JSON.parse(data)
+    if(products.some(product => product.code ===body.code)){
+        return res.status(200).json({error:"producto ya existente"})
+    }
+
+    const newProduct = {
+        id: crypto.randomUUID(),
+        ...body
+    }
+    products.push(newProduct)
+    await fs.promises.writeFile('products.json',JSON.stringify(products,null,2))
+    res.json({message:"Producto creado exitosamente"})
+})
+
 
 export default router
